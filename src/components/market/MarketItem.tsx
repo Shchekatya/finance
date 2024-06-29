79c4adc4fb102f290faa700cc15c2a7702532ded
Shchekatya <shchekatya@gmail.com>
2024-06-29 23:50:44 +0300

    feat: added charts

--- src/components/market/MarketItem.tsx
+++ src/components/market/MarketItem.tsx
@@ -1,10 +1,23 @@
 import { useParams } from "react-router-dom";
 import { Title } from "../common/Title";
+import { Chart } from "./Chart";
+import styled from 'styled-components'
 
 export const MarketItem = () => {
   const { slug } = useParams()
 
   return (
-    <Title>MarketItem {slug}</Title>
+    <>
+      <Title>MarketItem {slug}</Title>
+      <ChartView>
+        <Chart />
+      </ChartView>
+    </>
   );
 }
+
+const ChartView = styled.div`
+  width: 80%;
+  margin: 0 auto;
+`
+
